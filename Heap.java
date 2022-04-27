482d83da87cfd4021fabd5efdd64ac985e14824e
H4k7g3 <94833372+H4k7g3@users.noreply.github.com>
2022-04-27 22:31:15 +0600

    Add files via upload

---
+++ Heap.java
@@ -0,0 +1,95 @@
+public class Heap<T extends Comparable<T>> {
+
+    private MyArrayList<T> list;
+    final private int root = 0;
+
+    private void heapify(int index)
+    {
+        int min   = index;
+        int left  = 2 * index + 1;
+        int right = 2 * index + 2;
+
+        T minEl = list.get(min);
+        T leftEl = leftChildOf(index);
+        T rightEl = rightChildOf(index);
+
+        if (right <= (size()-1) && rightEl.compareTo(minEl) < 0)
+            min = right;
+
+        if (left <= (size()-1) && leftEl.compareTo(minEl) < 0)
+            min = left;
+
+        if (min != index) {
+            swap(index, min);
+            heapify(min);
+            traverseUp(index);
+        }
+    }
+
+
+    public Heap() {
+        list = new MyArrayList<>();
+    }
+
+    public boolean remove(T item){
+        if (list.contains(item)){
+            swap(list.indexOf(item), size()-1);
+            boolean removed = list.remove(item);
+            heapify(0);
+            return removed;
+        } else return false;
+    }
+
+    public void add(T item) {
+        list.add(item);
+        traverseUp(list.indexOf(item));
+    }
+
+    public boolean isEmpty() {
+        return list.size() == 0;
+    }
+    private void traverseUp(int index){
+        if (list.indexOf(parentOf(index)) == root){
+            if(list.get(index).compareTo(min()) < 0){
+                swap(root, index);
+                return;
+            }
+        } else {
+            int current = index;
+            int parent = list.indexOf(parentOf(index));
+
+            while ((list.get(index)).compareTo(parentOf(current)) < 0) {
+                swap(current, parent);
+            }
+            traverseUp(parent);
+        }
+    }
+
+
+    private void swap(int post, int dest) {
+        list.swap(post, dest);
+    }
+
+    private T rightChildOf(int index) {
+        return list.get(index*2 + 2);
+    }
+
+    private T leftChildOf(int index) {
+        return list.get(index*2 + 1);
+    }
+
+    private T parentOf(int index){
+        return list.get((index-1)/2);
+    }
+    public T min(){
+        if(list.size() != 0)
+            return list.get(root);
+        return null;
+    }
+
+    public int size(){
+        if(list.size() != 0)
+            return list.size();
+        return -1;
+    }
+}
