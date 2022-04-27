a32b5ba0eb93445ddc1da8b10b1436db6d491bdc
H4k7g3 <94833372+H4k7g3@users.noreply.github.com>
2022-04-27 20:11:59 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,101 +1,19 @@
+import java.util.Scanner;
+
+
 public class Main {
-    public static void main (String[] args){
-       MyArrayList<Integer> list = new MyArrayList<Integer>();
-        list.add(1);
-        list.add(2);
-        list.add(0);
-        list.add(-7);
-        list.add(4);
-        list.add(1);
-        list.add(5);
+    public static void main (String[] args) throws Exception {
 
-        System.out.println(list.size());
-
-        for(int i=0;i<list.size();i++){
-            System.out.print(list.get(i)+" ");
-        }
-
-        System.out.println();
-
-        System.out.println(list.contains(1));
-
-        System.out.println(list.get(2));
-
-        System.out.println(list.indexOf(5));
-
-        System.out.println(list.lastIndexOf(1));
-
-        System.out.println (list.remove(3));
-
-        list.add(4,2);
-
-        for(int i=0;i<list.size();i++){
-            System.out.print(list.get(i)+" ");
-        }
-
-        System.out.println();
-
-        System.out.println(list.remove((Integer)3));
-        System.out.println(list.remove((Integer)2));
-
-        for(int i=0;i<list.size();i++){
-            System.out.print(list.get(i)+" ");
-        }
-
-        System.out.println();
-
-        list.sort();
-
-        for(int i=0;i<list.size();i++){
-            System.out.print(list.get(i)+" ");
-        }
-
-        System.out.println();
-
-        MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
-        myLinkedList.add(1);
-        myLinkedList.add(2);
-        myLinkedList.add(0);
-        myLinkedList.add(-5);
-        myLinkedList.add(5, 2);
-        myLinkedList.add(4);
-        myLinkedList.add(3);
-
-        System.out.println("-------------------------");
-
-        int i = 0;
-        while(i != myLinkedList.size()){
-            System.out.print(myLinkedList.get(i)+" ");
-            i++;
-        }
-
-        System.out.println("");
-        System.out.println(myLinkedList.lastIndexOf(1));
-        System.out.println(myLinkedList.indexOf(2));
-        System.out.println(myLinkedList.contains(3));
-
-        int j =0;
-
-        myLinkedList.sort();
-        i = 0;
-        while(i != myLinkedList.size()){
-            System.out.print(myLinkedList.get(i)+" ");
-            i++;
-        }
-
-        System.out.println("");
-
-
-        System.out.println(myLinkedList.remove(5));
-        
-        Stack<Integer> stack = new Stack<Integer>();
+     Stack<Integer> stack = new Stack<Integer>();
      Scanner input = new Scanner(System.in);
      String s;
-     System.out.println("Max stack size is 20");
+     System.out.println("You can enter the values of stack | to exit from stack write 'stop' ");
+     System.out.println("To use pop: write 'null' ");
 
-     i = 0;
+     int i = 0;
      while (input.hasNext()) {
       s = input.next();
+      if (s.equals("stop"))break;
       if (s.equals("null")){
        System.out.println(stack.pop()+" ");
       } else {
@@ -103,6 +21,23 @@ public class Main {
       }
      }
 
-        
-        }
+     System.out.println(stack.peek());
+
+     System.out.println("----------");
+
+     Queue<Integer> queue = new Queue<Integer>();
+     queue.add(5);
+     queue.add(2);
+     queue.add(4);
+     queue.add(1);
+
+     System.out.println(queue.size());
+     System.out.println(queue.isEmpty());
+     System.out.println(queue.peek());
+     System.out.println(queue.offer(5));
+
+
+
+
     }
+}
